9ff8f809506d8354fe6fb85fa1acb119d221a00b
Mayank Deep <mayankdeep217@gmail.com>
2024-04-09 23:13:31 +0530

    added new files

---
+++ push method details/pushMethod.js
@@ -0,0 +1,22 @@
+let arr = [1 , 3 ,4 ,5 , 6 ,7 ,7, 89 , "name" , "null" , undefined , true , false];
+
+// console.log(arr);
+
+// push will push an element into the last of the array 
+
+// arr.push("pushed by push method");
+// console.log(arr);
+
+// we can push multiple elements at once using push method 
+
+let array = [1 , "mk" , "sugar"];
+console.log(array);
+
+array.push(34 , 56 , 8 , "custom elemnts" , "car" , "bike");
+console.log(array);
+
+
+let arr2 = [];
+let arr3 = [];
+
+console.log(arr2 == arr3);      // false because it will not compare the elements inside but the address of these arrays
